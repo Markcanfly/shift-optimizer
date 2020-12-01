674051d8e1b7988921a37dbb8dfe3cd7131eb1e5
Mark Varga <varga.mrk@gmail.com>
2020-12-01 19:57:42 +0100

    Fix preference generator

--- generate_preferences.py
+++ generate_preferences.py
@@ -1,18 +1,37 @@
 from random import sample, randint, choice, shuffle
+from data import flat_shifts
 
-n_people = 70
-days = list(range(7))
-# TODO fix generator - doesn't take into account how many shifts are on a specific day
-def get_requests(n_people, n_shifts, days_per_person, shifts_per_person):
-    requests = list() # Dictionary of days of applications
+def generate_requests(n_people, days_per_person, shifts_per_day):
+    shifts = dict()
+    for shift in flat_shifts:
+        day = shift[0]
+        shifts[day] = set()
+    
+    for shift in flat_shifts:
+        day = shift[0]
+        shifts[day].add(shift)
+    # Shifts is now a shift['day'] = ((id, day, ...), (id2, day2,...)) dict
+    
+    requests = set() # Dictionary of days of applications
     for p in range(n_people):
-        for d in sample(range(7), days_per_person): # Choose
-            shifts_applied_to = set() # At least one long shift has to be already in the set
-            shifts_applied_to.add(choice((0,7)))
-            for shift in sample(range(n_shifts), shifts_per_person):
-                shifts_applied_to.add(shift)
-            preference_list = list(shifts_applied_to)
-            shuffle(preference_list) # put the list in random order
-            for pref_score, shift_id in enumerate(preference_list):
-                requests.append((d, shift_id, p, pref_score)) # TODO solve preference order
+        days = sample(shifts.keys(), days_per_person)
+        for day in days:
+            shiftlist = shifts[day]
+            # Find a long shift
+            long_shifts = set()
+            for id_, d, capacity, begin, end in shiftlist:
+                del d, capacity # We don't care about capacity here, and day is redundant
+                if end-begin >= 5*60: # Shift is longer than 5 hours
+                    long_shifts.add(id_)
+            
+            temp_applied = set() # Shift ids that this person applies to
+            temp_applied.add(choice(list(long_shifts))) # Apply to a long shift on the day
+
+            for shift in sample(shiftlist, shifts_per_day):
+                temp_applied.add(shift[1]) # Apply to other shifts
+            
+            applied = list(temp_applied)
+            shuffle(applied) # Random order will indicate preference
+            for pref, s_id in enumerate(applied):
+                requests.add((day, s_id, p, pref))
     return requests

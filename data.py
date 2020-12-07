2fd7cb3c1750ab89e2c95893ca1cbd30a65ba012
Mark Varga <varga.mrk@gmail.com>
2020-12-07 23:42:15 +0100

    Update static data

--- data.py
+++ data.py
@@ -8,57 +8,57 @@ day_names = ['Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat', 
 shifts = [
     Shift(0, 0, beginning=Time(8, 45), end=Time(16,00)),
     Shift(1, 0, beginning=Time(11, 00), end=Time(15,00), capacity=2),
-    Shift(2, 0, beginning=Time(12, 00), end=Time(16,00), capacity=2), 
-    Shift(4, 0, beginning=Time(11, 30), end=Time(14,00), capacity=2),
+    Shift(2, 0, beginning=Time(12, 00), end=Time(17,00)), 
+    Shift(4, 0, beginning=Time(11, 30), end=Time(14,00)),
     Shift(5, 0, beginning=Time(17, 00), end=Time(22,00)), 
     Shift(6, 0, beginning=Time(18, 00), end=Time(21,30)), 
     Shift(7, 0, beginning=Time(17, 00), end=Time(23,45)),
 
     Shift(0, 1, beginning=Time(8, 45), end=Time(16,00)),
     Shift(1, 1, beginning=Time(11, 00), end=Time(15,00), capacity=2),
-    Shift(2, 1, beginning=Time(12, 00), end=Time(16,00), capacity=2), 
-    Shift(4, 1, beginning=Time(11, 30), end=Time(14,00), capacity=2),
+    Shift(2, 1, beginning=Time(12, 00), end=Time(17,00)), 
+    Shift(4, 1, beginning=Time(11, 30), end=Time(14,00)),
     Shift(5, 1, beginning=Time(17, 00), end=Time(22,00)), 
     Shift(6, 1, beginning=Time(18, 00), end=Time(21,30)), 
     Shift(7, 1, beginning=Time(17, 00), end=Time(23,45)),
 
     Shift(0, 2, beginning=Time(8, 45), end=Time(16,00)),
     Shift(1, 2, beginning=Time(11, 00), end=Time(15,00), capacity=2),
-    Shift(2, 2, beginning=Time(12, 00), end=Time(16,00), capacity=2), 
-    Shift(4, 2, beginning=Time(11, 30), end=Time(14,00), capacity=2),
+    Shift(2, 2, beginning=Time(12, 00), end=Time(17,00)), 
+    Shift(4, 2, beginning=Time(11, 30), end=Time(14,00)),
     Shift(5, 2, beginning=Time(17, 00), end=Time(22,00)), 
     Shift(6, 2, beginning=Time(18, 00), end=Time(21,30)), 
     Shift(7, 2, beginning=Time(17, 00), end=Time(23,45)),
 
     Shift(0, 3, beginning=Time(8, 45), end=Time(16,00)),
     Shift(1, 3, beginning=Time(11, 00), end=Time(15,00), capacity=2),
-    Shift(2, 3, beginning=Time(12, 00), end=Time(16,00), capacity=2), 
-    Shift(4, 3, beginning=Time(11, 30), end=Time(14,00), capacity=2),
+    Shift(2, 3, beginning=Time(12, 00), end=Time(17,00)), 
+    Shift(4, 3, beginning=Time(11, 30), end=Time(14,00)),
     Shift(5, 3, beginning=Time(17, 00), end=Time(22,00)), 
     Shift(6, 3, beginning=Time(18, 00), end=Time(21,30)), 
     Shift(7, 3, beginning=Time(17, 00), end=Time(23,45)),
 
     Shift(0, 4, beginning=Time(8, 45), end=Time(16,00)),
     Shift(1, 4, beginning=Time(11, 00), end=Time(15,00), capacity=2),
-    Shift(2, 4, beginning=Time(12, 00), end=Time(16,00), capacity=2), 
-    Shift(4, 4, beginning=Time(11, 30), end=Time(14,00), capacity=2),
+    Shift(2, 4, beginning=Time(12, 00), end=Time(17,00)), 
+    Shift(4, 4, beginning=Time(11, 30), end=Time(14,00)),
     Shift(5, 4, beginning=Time(17, 00), end=Time(22,00)), 
     Shift(6, 4, beginning=Time(18, 00), end=Time(21,30)), 
     Shift(7, 4, beginning=Time(17, 00), end=Time(23,45)),
 
     Shift(0, 5, beginning=Time(8, 45), end=Time(16,00)),
-    Shift(1, 5, beginning=Time(11, 00), end=Time(15,00), capacity=2),
-    Shift(3, 5, beginning=Time(12, 00), end=Time(17,00)), 
-    Shift(4, 5, beginning=Time(17, 00), end=Time(22,00)), 
-    Shift(5, 5, beginning=Time(18, 00), end=Time(21,30)), 
-    Shift(6, 5, beginning=Time(17, 00), end=Time(23,45)),
+    Shift(1, 5, beginning=Time(11, 00), end=Time(15,00)),
+    Shift(2, 5, beginning=Time(12, 00), end=Time(17,00)), 
+    Shift(5, 5, beginning=Time(17, 00), end=Time(22,00)), 
+    Shift(6, 5, beginning=Time(18, 00), end=Time(21,30), capacity=2), 
+    Shift(7, 5, beginning=Time(17, 00), end=Time(23,45)),
 
     Shift(0, 6, beginning=Time(8, 45), end=Time(16,00)),
-    Shift(1, 6, beginning=Time(11, 00), end=Time(15,00), capacity=2),
-    Shift(3, 6, beginning=Time(12, 00), end=Time(17,00)), 
-    Shift(4, 6, beginning=Time(17, 00), end=Time(22,00)), 
-    Shift(5, 6, beginning=Time(18, 00), end=Time(21,30)), 
-    Shift(6, 6, beginning=Time(17, 00), end=Time(23,45))
+    Shift(1, 6, beginning=Time(11, 00), end=Time(15,00)),
+    Shift(2, 6, beginning=Time(12, 00), end=Time(17,00)), 
+    Shift(5, 6, beginning=Time(17, 00), end=Time(22,00)), 
+    Shift(6, 6, beginning=Time(18, 00), end=Time(21,30), capacity=2), 
+    Shift(7, 6, beginning=Time(17, 00), end=Time(23,45)),
 ]
 
 flat_shifts = []
@@ -101,5 +101,3 @@ if __name__ == "__main__":
     
     with open('shifts.json', 'w', encoding='utf8') as jsonfile:
         json.dump(shift_dict, jsonfile, indent=4, ensure_ascii=False)
-
-    from_csv()

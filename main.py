0783f98da3f417a1c8cc18802123a84112be0670
Mark Varga <varga.mrk@gmail.com>
2020-12-25 22:12:46 +0100

    CLI to excel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import data
 from solver import ShiftSolver
+import excel
 
 parser = argparse.ArgumentParser()
 parser.add_argument('prefs', help='Name of the csv file containing the user-submitted preferences.', type=str)
@@ -8,10 +9,15 @@ parser.add_argument('shifts', help='Name of the json file containing the shifts.
 parser.add_argument('groups', help='Name of the json file containing the groups', type=str)
 parser.add_argument('-t', '--timeout', help='The maximum time in seconds that the solver can take to find an optimal solution.', default=10, type=int)
 parser.add_argument('-v', '--verbose', help='Print some extra data about the solution.', action='store_true')
+parser.add_argument('-o', dest='outxlsx', help='Output to an excel file.', type=str)
 # TODO outfiles
 args = parser.parse_args()
 
-solver = ShiftSolver(shifts=data.shifts_from_json(args.shifts), preferences=data.preferences_from_csv(args.prefs))
+# Collect data from files
+stuples = data.shifts_from_json(args.shifts)
+preftuples = data.preferences_from_csv(args.prefs)
+
+solver = ShiftSolver(shifts=stuples, preferences=preftuples)
 for min_workers in (1, 0): 
     if solver.Solve(
         min_workers=min_workers, 
@@ -21,5 +27,9 @@ for min_workers in (1, 0):
         print(solver.get_overview())
         if args.verbose:
             print(f'Unhappiness value: {solver.ObjectiveValue()} (lower is better) in {round(solver.WallTime(), 2)} seconds.')
+        
+        if args.outxlsx:
+            excel.write_to_file(args.outxlsx, stuples, preftuples)
+
         break
     

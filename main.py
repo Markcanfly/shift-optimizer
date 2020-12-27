04ac0dfcf3cf6ccf9931878ce0889a64933201eb
Mark Varga <varga.mrk@gmail.com>
2020-12-27 13:54:54 +0100

    Parameterize min_workers

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('prefs', help='Name of the CSV file containing the user-submitted preferences.', type=str)
 parser.add_argument('shifts', help='Name of the JSON file containing the shifts.', type=str)
 parser.add_argument('groups', help='Name of the JSON file containing the groups', type=str)
-parser.add_argument('-p', dest='min_shifts_filled', action=PercentageValidatorAction, default=0, help='The percentage of shifts the model has to fill.', type=float)
+parser.add_argument('-w', dest='min_workers_per_shift', default=0, help='The minimum number of workers on all shifts.', type=int)
+parser.add_argument('-p', dest='min_shifts_filled_percent', action=PercentageValidatorAction, default=0, help='The percentage of shifts the model has to fill.', type=float)
 parser.add_argument('-t', '--timeout', help='The maximum time in seconds that the solver can take to find an optimal solution.', default=10, type=int)
 parser.add_argument('-v', '--verbose', help='Print some extra data about the solution.', action='store_true')
 parser.add_argument('-o', dest='outjson', help='Output the raw model output to a JSON file.')
@@ -31,22 +32,20 @@ prefs = data.preferences_from_csv(args.prefs, args.shifts)
 personal_reqs = data.personal_reqs_from_groups(args.groups)
 
 solver = ShiftSolver(shifts=shifts, preferences=prefs, personal_reqs=personal_reqs)
-for min_workers in (1, 0): 
-    if solver.Solve(
-        min_workers=min_workers, 
-        timeout=args.timeout,
-        min_shifts_filled_ratio=args.min_shifts_filled
-                    ):
-        print(solver.get_overview())
-        if args.verbose:
-            print(f'Unhappiness value: {solver.ObjectiveValue()} (lower is better) in {round(solver.WallTime(), 2)} seconds.')
-        if args.outjson:
-            with open(args.outjson, 'w', encoding='utf8') as jsonfile:
-                json.dump(data.json_compatible_solve(solver.get_values()), jsonfile, indent=4, ensure_ascii=False)
-        
-        if args.outxlsx:
-            assignments = solver.get_values()
-            excel.write_to_file(args.outxlsx, shifts, prefs, assignments, personal_reqs)
+if solver.Solve(
+    min_workers=args.min_workers_per_shift, 
+    timeout=args.timeout,
+    min_shifts_filled_ratio=args.min_shifts_filled_percent
+                ):
+    print(solver.get_overview())
+    if args.verbose:
+        print(f'Unhappiness value: {solver.ObjectiveValue()} (lower is better) in {round(solver.WallTime(), 2)} seconds.')
+    if args.outjson:
+        with open(args.outjson, 'w', encoding='utf8') as jsonfile:
+            json.dump(data.json_compatible_solve(solver.get_values()), jsonfile, indent=4, ensure_ascii=False)
+    
+    if args.outxlsx:
+        assignments = solver.get_values()
+        excel.write_to_file(args.outxlsx, shifts, prefs, assignments, personal_reqs)
 
-        break
     

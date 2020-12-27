291e11c856dfb545eab6f3a453dbf5a726a95114
Mark Varga <varga.mrk@gmail.com>
2020-12-27 13:52:28 +0100

    Add option to fill a percentage of shifts

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ import json
 from solver import ShiftSolver
 import excel
 
+class PercentageValidatorAction(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed")
+        super(PercentageValidatorAction, self).__init__(option_strings, dest, **kwargs)
+    def __call__(self, parser, namespace, value, option_string=None):
+        if not (0 <= value <= 100):
+            raise ValueError("Percentage has to be between 0 and 100 (inclusive)")
+        setattr(namespace, self.dest, value/100)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('prefs', help='Name of the CSV file containing the user-submitted preferences.', type=str)
 parser.add_argument('shifts', help='Name of the JSON file containing the shifts.', type=str)
 parser.add_argument('groups', help='Name of the JSON file containing the groups', type=str)
+parser.add_argument('-p', dest='min_shifts_filled', action=PercentageValidatorAction, default=0, help='The percentage of shifts the model has to fill.', type=float)
 parser.add_argument('-t', '--timeout', help='The maximum time in seconds that the solver can take to find an optimal solution.', default=10, type=int)
 parser.add_argument('-v', '--verbose', help='Print some extra data about the solution.', action='store_true')
 parser.add_argument('-o', dest='outjson', help='Output the raw model output to a JSON file.')
@@ -23,7 +34,8 @@ solver = ShiftSolver(shifts=shifts, preferences=prefs, personal_reqs=personal_re
 for min_workers in (1, 0): 
     if solver.Solve(
         min_workers=min_workers, 
-        timeout=args.timeout
+        timeout=args.timeout,
+        min_shifts_filled_ratio=args.min_shifts_filled
                     ):
         print(solver.get_overview())
         if args.verbose:

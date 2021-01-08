6f78fd3ffe9dea88a5470a99ad55cd1ac67d4b67
Mark Varga <varga.mrk@gmail.com>
2021-01-08 22:39:59 +0100

    Add max workdays constraint

--- solver.py
+++ solver.py
@@ -63,6 +63,20 @@ class ShiftModel(cp_model.CpModel):
             for s in shifts:
                 self.AddLinearConstraint(sum([self.variables[(d, s, p)] for p in self.people]), min, self.shift_data[(d,s)][0])
 
+    def AddMaxWorkdays(self, n):
+        """Make sure that nobody works on more than a given number of days.
+        Args:
+            n: the maximum number of words
+        """
+        for p in self.people:
+            day_shift_variables = dict()
+            for d in self.shifts_for_day:
+                # Assign the daily boolvars
+                day_shift_variables[d] = [self.variables[d,s,p] for s in self.shifts_for_day[d]]
+            # Sum of works_on_day for all days is 0<x<n
+            self.AddLinearConstraint(sum(max(day_shift_variables[d]) for d in self.shifts_for_day), 0, n)
+
+
     def AddMaxDailyShifts(self, n):
         """Make sure that employees only get assigned to
         maximum of n shifts on any given day.
@@ -345,6 +359,7 @@ class ShiftSolver(cp_model.CpSolver):
         self.__model.AddMinimumFilledShiftRatio(ratio=min_capacities_filled_ratio)
         self.__model.MaximizeWelfare(pref_function)
         self.__model.AddMaxDailyShifts(1)
+        self.__model.AddMaxWorkdays(5)
         self.parameters.max_time_in_seconds = timeout
         super().Solve(self.__model)
         if super().StatusName() in ('FEASIBLE', 'OPTIMAL'):

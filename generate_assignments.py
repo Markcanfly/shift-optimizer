f983d088b2bf859303f42be213ef48682dfb71a7
Mark Varga <varga.mrk@gmail.com>
2021-01-11 19:27:58 +0100

    Add option to get an overview without solve

--- generate_assignments.py
+++ generate_assignments.py
@@ -9,6 +9,7 @@ from copy import deepcopy
 parser = argparse.ArgumentParser()
 parser.add_argument('dirpath', help='The path of the directory of the inputsite. This should contain the js file with the shifts.', type=str)
 parser.add_argument('urlname', help='The name of the form in the directory.', type=str)
+parser.add_argument('--no-solve', dest='nosolve', help="Don't solve, just create the overview excel.", action='store_true')
 parser.add_argument('-t', '--timeout', help='The maximum time in seconds that the solver can take to find an optimal solution.', default=None, type=int)
 args = parser.parse_args()
 
@@ -27,24 +28,27 @@ starting_capacity = int(sum_capacities*0.85)
 Path(args.urlname+'/sols').mkdir(parents=True, exist_ok=True)
 rows = [] # {'pref':s1, 'unfilled':s2, 'empty':s3, 'filename':s4}
 
-for n in range(starting_capacity, sum_capacities+1):
-    if solver.Solve(
-            min_workers=0,
-            timeout=args.timeout,
-            min_capacities_filled=n):       
-        print(f'Prefscore: {solver.ObjectiveValue()} Completely empty shifts: {solver.EmptyShifts()} Unfilled capacities: {solver.UnfilledCapacities()} in {round(solver.WallTime(),2)} seconds', end='')
-        if solver.StatusName() != 'OPTIMAL':
-            print(' !SUBOPTIMAL SOLVE! Try to run with more time', end='')
-        print()
-        xlsxsubpath = f'sols/{n}.xlsx'
-        xlsxfilepath = f'{args.urlname}/{xlsxsubpath}'
-        # Write to excel and add index for the root later
-        rows.append((xlsxsubpath, deepcopy(solver)))
-        excel.write_to_file(xlsxfilepath, shifts, prefs, solver.Values(), personal_reqs)
-    else: # No more solutions to be found
-        break
+if not args.nosolve:
+    for n in range(starting_capacity, sum_capacities+1):
+        if solver.Solve(
+                min_workers=0,
+                timeout=args.timeout,
+                min_capacities_filled=n):       
+            print(f'Prefscore: {solver.ObjectiveValue()} Completely empty shifts: {solver.EmptyShifts()} Unfilled capacities: {solver.UnfilledCapacities()} in {round(solver.WallTime(),2)} seconds', end='')
+            if solver.StatusName() != 'OPTIMAL':
+                print(' !SUBOPTIMAL SOLVE! Try to run with more time', end='')
+            print()
+            xlsxsubpath = f'sols/{n}.xlsx'
+            xlsxfilepath = f'{args.urlname}/{xlsxsubpath}'
+            # Write to excel and add index for the root later
+            rows.append((xlsxsubpath, deepcopy(solver)))
+            excel.write_to_file(xlsxfilepath, shifts, prefs, solver.Values(), personal_reqs)
+        else: # No more solutions to be found
+            break
 
-if len(rows) > 0:
-    excel.write_summary(f'{args.urlname}/solindex.xlsx', rows)
+    if len(rows) > 0:
+        excel.write_summary(f'{args.urlname}/solindex.xlsx', rows)
+else: # Nosolve invoked
+    excel.write_to_file(f'{args.urlname}.xlsx', shifts, prefs, data.empty_assignments(shifts, prefs), personal_reqs)
 
-    
+
